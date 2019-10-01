adf9a42fccd73228c07049f55bd918fbf59c6216
paolomab <53303346+paolomab@users.noreply.github.com>
2019-10-01 08:46:48 +0800

    Update Birthday.java

--- Birthday.java
+++ Birthday.java
@@ -1,45 +1,32 @@
-/**
- * @(#)Birthday.java
- *
- *
- * @author 
- * @version 1.00 2019/9/3
- */
-
-
 import java.util.Date;
-import java.util.Scanner;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
-public class Birthday 
-{
+public class Activity1 {
 
     public static void main(String []args) throws Exception
     {
     	Scanner sc = new Scanner(System.in);
-    	
-    	SimpleDateFormat format=new SimpleDateFormat("dd-M-yyyy");
-    	
-    	System.out.print("input Date: ");
-    	String input_date = sc.nextLine();
-    	Date date = format.parse(input_date);
-    	
-    	System.out.println(date);
-    	
-    	Calendar calendar = new GregorianCalendar();
-    	
-    	String input_bday = "13-11-2000";
-    	Date bday = format.parse(input_bday);
-    	
-    	int year = calendar.get(Calendar.YEAR);
-    	calendar.setTime(bday);
-    	int byear = calendar.get(Calendar.YEAR);
-    	
-    	int age = year - byear;
-    	System.out.print("My age " +age);
+    		
+    	Date date = new Date();
+   		SimpleDateFormat format=new SimpleDateFormat ("dd-MM-yyyy");
+   		Calendar calendar = new GregorianCalendar();
+   		
+   		System.out.println("Enter your Birthday below");
+   		String input_bday = sc.nextLine();
+   		Date bday = format.parse(input_bday);
+   		
+   		int year = calendar.get(Calendar.YEAR);
+   		calendar.setTime(bday);
+   		int byear = calendar.get(Calendar.YEAR);
+   		
+   		int age = year - byear;
+   		System.out.print("My age: " + age);
+   		
+   		
+   		
     }
-    
     
 }
